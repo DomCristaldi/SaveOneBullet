cc0a3149ea9ad6194639af0b5ff828f48f5c1dc4
Justin Cirigliano <cirigj@rpi.edu>
2015-09-21 21:25:15 -0400

    ++ Maze Controller can now spawn a player from prefab ++ More debug keys ++ Added NodeTracker script, which automatically updates a reference to the closest node ++ Added a NodeTracker to the player prefab

---
+++ SaveOneBulletGame/Assets/MazeGen/NodeTracker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Scripts/MazeGen/NodeTracker")]
+public class NodeTracker : MonoBehaviour {
+    
+	public MazeNode closestNode;
+	bool tracking;
+
+    void Awake () {
+		closestNode = null;
+		tracking = true;
+    }
+    
+	void Start () {
+        if (MazeController.singleton == null) {
+			tracking = false;
+			Debug.LogError("No MazeController can be found!");
+		}
+	}
+	
+	void Update () {
+        if (tracking) {
+			closestNode = MazeController.singleton.ClosestNodeToPositon(transform.position);
+			Debug.DrawLine(transform.position, closestNode.transform.position, Color.red);
+		}
+	}
+}
