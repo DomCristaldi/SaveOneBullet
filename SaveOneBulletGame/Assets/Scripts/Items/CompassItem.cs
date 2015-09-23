91e664474c69ed84ea1d47f2581453f20116160d
DomCristaldi <domeniccristaldi@gmail.com>
2015-09-23 07:25:47 -0400

    compass looks good now

--- SaveOneBulletGame/Assets/Scripts/Items/CompassItem.cs
+++ SaveOneBulletGame/Assets/Scripts/Items/CompassItem.cs
@@ -4,28 +4,34 @@ using System.Collections;
 [AddComponentMenu("Scripts/Items/CompassItem")]
 public class CompassItem : ItemBase {
 
-	public GameObject needle;
+	public Transform needle;
 
 	protected override void Awake() {
 		base.Awake();
 		
 		thisItemType = ItemType.compass;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        pointToEscape();
 	}
 
 	//using the compass will bring the compass in front of you and will point to the way out
 	public override void Use() {
 		base.Use ();
-		pointToEscape ();
+		//pointToEscape ();
 	}
 
 	//function that will update the needle on the compass to point to escape
 	private void pointToEscape(){
+        Vector3 exitDirec = (MazeController.singleton.exit.transform.position - needle.position).normalized;
+        //needlePlane.SetNormalAndPosition(needle.up, needle.position);
 
+        Vector3 needleDirec = (Vector3.ProjectOnPlane(exitDirec, needle.up)).normalized;
+
+        needle.rotation = Quaternion.LookRotation(needleDirec, needle.up);
 	}
 
     public override void Equip() {
