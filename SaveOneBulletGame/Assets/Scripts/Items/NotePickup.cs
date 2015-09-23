56c85c02d2b7cd3e0026d1e70409051d5db35c1a
DomCristaldi <domeniccristaldi@gmail.com>
2015-09-23 04:33:24 -0400

    ++ Note Pickups should now work

--- SaveOneBulletGame/Assets/Scripts/Items/NotePickup.cs
+++ SaveOneBulletGame/Assets/Scripts/Items/NotePickup.cs
@@ -6,16 +6,24 @@ public class NotePickup : MonoBehaviour {
     public string noteText;
 
     void OnTriggerEnter(Collider other) {
-        InventoryController inv = other.GetComponent<InventoryController>();
+        InventoryController inv = other.transform.root.gameObject.GetComponent<InventoryController>();
 
+        //Debug.Log(inv.gameObject.name);
+
+        //Debug.Log(inv.name);
+        
+        
         if (!inv.ItemInInventory(ItemBase.ItemType.note)) {
 
         }
         else {
             NoteItem nItem = inv.equipSlots[ItemBase.ItemType.note].item as NoteItem;//upcast b/c we know it's going to be a note
 
+            nItem.AddNote(noteText);
 
+            Destroy(gameObject);
         }
+        
     }
 
 	// Use this for initialization
