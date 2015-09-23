0b32083ef4fbf2922022ba08dd8dc0011b513067
DomCristaldi <domeniccristaldi@gmail.com>
2015-09-23 03:54:34 -0400

    ++ Note Manager now gets the strings in all the Note text files ++ Inventory Controller: Added a function to check if the player has an item .. cleaned up the Inventory Controller .. reorganized where the Note text files are

---
+++ SaveOneBulletGame/Assets/Scripts/Items/NotePickup.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NotePickup : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other) {
+        InventoryController inv = other.GetComponent<InventoryController>();
+
+        if (!inv.ItemInInventory(ItemBase.ItemType.note)) {
+
+        }
+        else {
+            NoteItem nItem = inv.equipSlots[ItemBase.ItemType.note].item as NoteItem;//upcast b/c we know it's going to be a note
+
+
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
