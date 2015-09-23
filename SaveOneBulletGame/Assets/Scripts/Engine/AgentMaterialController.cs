f5a68fd97539aace2d4f1b20d2ce1021799a9e85
DomCristaldi <domeniccristaldi@gmail.com>
2015-09-23 12:25:54 -0400

    ++ Created a script that handles changing the dissolve amount for all the renderers on a Wraith

---
+++ SaveOneBulletGame/Assets/Scripts/Engine/AgentMaterialController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AgentMaterialController : MonoBehaviour {
+
+    public List<Renderer> renderList;
+    /*
+    [Range(0.0f, 1.0f)]
+    public float dissolveAmount = 0.0f;
+    */
+    void Awake() {
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+        //SetDissolveAmount(0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetDissolveAmount(float amount) {
+        foreach (Renderer ren in renderList) {
+            ren.material.SetFloat("_SliceAmount", amount);
+        }
+    }
+}
