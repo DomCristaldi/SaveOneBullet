7101e766769fdbce1c439ef9eefaff252640610d
DomCristaldi <domeniccristaldi@gmail.com>
2015-09-23 03:00:08 -0400

    NoteItem is funcitonal

---
+++ SaveOneBulletGame/Assets/Scripts/Items/NoteUIHandler.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NoteUIHandler : MonoBehaviour {
+
+    private Text uiText;
+
+    public string text;
+
+    void Awake() {
+        uiText = GetComponent<Text>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void AssignTextToUI() {
+        uiText.text = text;
+    }
+
+    public void SetText(TextAsset tAsset) {
+        this.text = tAsset.text;
+        AssignTextToUI();
+
+    }
+
+    public void  SetText(string s) {
+        text = s;
+        AssignTextToUI();
+
+    }
+}
