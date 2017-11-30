88571571abc6c0f3347ba81689d70ace1d8d306c
DomCristaldi <DomCristaldi@users.noreply.github.com>
2017-11-30 17:12:41 -0500

    Update Motor.cs
    cleaned up some comments

--- SaveOneBulletGame/Assets/Scripts/Engine/Motor.cs
+++ SaveOneBulletGame/Assets/Scripts/Engine/Motor.cs
@@ -33,10 +33,7 @@ public class Motor : MonoBehaviour {
             return tf.position + trueDirec.normalized;
         }
     }
-    /*
-    [Header("Speed")]
-    public float maxDesiredDirecMag = 2.0f;
-    */
+
     public float moveSpeedModifier = 1.0f;
 
     public float redirectSpeed = 0.5f;
@@ -46,12 +43,10 @@ public class Motor : MonoBehaviour {
         rigbod = GetComponent<Rigidbody>();
     }
 
-	// Use this for initialization
 	protected virtual void Start () {
 	
 	}
 	
-	// Update is called once per frame
 	protected virtual void Update () {
 	    HandleMovement();
 
@@ -67,16 +62,7 @@ public class Motor : MonoBehaviour {
     
 //HANDLE ANY MODIFICATIONS THE MOTOR HAS TO MAKE TO MOVEMENT
     protected virtual void HandleMovement() {
-        //trueDirec = desiredDirec;
-
-        //Vector3 newDirec = Vector3.MoveTowards(truePoint, desiredPoint, redirectSpeed) - tf.position;
-
-        //trueDirec = newDirec * (maxDesiredDirecMag * moveSpeedModifier);
-        //trueDirec = newDirec * moveSpeedModifier;
-
         trueDirec = Vector3.MoveTowards(tf.position + trueDirec, tf.position + desiredDirec, redirectSpeed * Time.deltaTime) - tf.position;
-
-        //rigbod.velocity = new Vector3(trueDirec.x, rigbod.velocity.y, trueDirec.z);
     }
 
 //APPLY THE MOVEMENT TO THE RIGID BODY
